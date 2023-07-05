2f3966f14eb21d22317b866610c58b33d6021a25
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-07-05 09:22:21 +0300

    Networking

--- app/src/main/java/flo/app/myshop/MainActivity.kt
+++ app/src/main/java/flo/app/myshop/MainActivity.kt
@@ -2,15 +2,24 @@ package flo.app.myshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import flo.app.myshop.databinding.ActivityMainBinding
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    var products: List<Product> = emptyList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 
     override fun onResume() {
@@ -18,12 +27,16 @@ class MainActivity : AppCompatActivity() {
         getProducts()
     }
     fun getProducts(){
-        val retrofit = ApiClient.buildClient(ApiInterface::class.java)
-        val request = retrofit.getProducts()
-        request.enqueue(object : Callback<List<Product>>{
-            override fun onResponse(call: Call<List<Product>>, response: Response<List<Product>>) {
+        val ApiClient = ApiClient.buildClient(ApiInterface::class.java)
+        val request = ApiClient.getProducts()
+        request.enqueue(object : Callback<ProductResponse>{
+            override fun onResponse(call: Call<ProductResponse>, response: Response<ProductResponse>) {
                 if(response.isSuccessful){
-                    var products = response.body()
+                    var products = response.body()?.products
+                    var adapter = ProductAdapter(products ?: emptyList())
+                    binding.rvProductList.layoutManager = LinearLayoutManager(this@MainActivity)
+                    binding.rvProductList.adapter = adapter
+
                     Toast.makeText(baseContext, "fetched ${products?.size} products", Toast.LENGTH_LONG).show()
                 }
                 else{
@@ -31,7 +44,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
 
-            override fun onFailure(call: Call<List<Product>>, t: Throwable) {
+            override fun onFailure(call: Call<ProductResponse>, t: Throwable) {
                 Toast.makeText(baseContext, t.message, Toast.LENGTH_LONG).show()
             }
         })
